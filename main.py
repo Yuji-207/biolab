c2224047faa259813001d77fd931ba7aaf43a56b
Nakamura Yuji <57951904+Yuji-207@users.noreply.github.com>
2022-07-15 10:02:06 +0900

    experimentsのcontorollerを実装

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import glob
+import json
+from fastapi import FastAPI
 from typing import List, Union
 from pydantic import BaseModel
+
+app = FastAPI()
 
 
 class Image(BaseModel):
@@ -58,3 +63,85 @@ class Experiment(BaseModel):
     conditions: List[Condition] = None
     start_date: str = None
     end_date: str = None
+
+
+@app.get('/experiments')
+async def get_experiments():
+
+    experiments = []
+    for path in glob.iglob('experiments/*'):
+        with open(path) as f:
+            experiment = json.load(f)
+            experiments.append(experiment)
+    
+    return experiments
+
+
+@app.post('/experiments')
+async def update_experiment(experiment: Experiment):
+
+    experiment = experiment.dict()
+
+    paths = glob.iglob('experiments/*')
+    paths = [int(path[12:-5]) for path in paths]
+    id = max(paths) + 1 if len(paths) else 0
+
+    with open(f'experiments/{id}.json', 'x') as f:
+        json.dump(experiment, f, indent=2)
+
+    return experiment
+
+
+@app.get('/experiments/{id}')
+async def get_experiments(id: int):
+
+    with open(f'experiments/{id}.json') as f:
+        experiment = json.load(f)
+    
+    return experiment
+
+
+@app.put('/experiments/{id}')
+async def get_experiments(id: int, experiment: Experiment):
+
+    experiment = experiment.dict()
+
+    with open(f'experiments/{id}.json', 'w') as f:
+        json.dump(experiment, f, indent=2)
+
+    return experiment
+
+
+@app.post('/experiments')
+async def update_experiment(experiment: Experiment):
+
+    experiment = experiment.dict()
+
+    paths = glob.iglob('experiments/*')
+    paths = [int(path[12:-5]) for path in paths]
+    id = max(paths) + 1 if len(paths) else 0
+
+    with open(f'experiments/{id}.json', 'x') as f:
+        json.dump(experiment, f, indent=2)
+
+    return experiment
+
+
+@app.get('/experiments/{id}')
+async def get_experiments(id: int):
+
+    with open(f'experiments/{id}.json') as f:
+        experiment = json.load(f)
+    
+    return experiment
+
+
+@app.put('/experiments/{id}')
+async def get_experiments(id: int, experiment: Experiment):
+
+    experiment = experiment.dict()
+
+    with open(f'experiments/{id}.json', 'w') as f:
+        json.dump(experiment, f, indent=2)
+
+    return experiment
